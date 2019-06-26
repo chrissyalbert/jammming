604cc84e4eb007404a5d52a59e86ea45419544c5
chrissyalbert <chrissy_albert@yahoo.com>
2019-06-26 16:11:42 -0700

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 let userAccessToken = '';
+const redirectURI = 'http://localhost:3000/';
 
 class Spotify extends React.Component {
   getAccessToken(userAccessToken) {
@@ -12,7 +13,9 @@ class Spotify extends React.Component {
       let expirationTime = window.location.href.match(/expires_in=([^&]*)/);
       window.setTimeout(() => userAccessToken = '', expirationTime * 1000);
       window.history.pushState('Access Token', null, '/');  
-    } 
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+    }
   }
 }
 
